4afeca08e99d33badd13819fcdf0b019561ff02b
Luigi Cham <luigiccham@gmail.com>
2014-09-20 07:29:24 +0800

    Created Project Solution

---
+++ t09-1c_main/t09-1c_main.cpp
@@ -0,0 +1,14 @@
+// t09-1c_main.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	system("pause");
+	return 0;
+}
+
