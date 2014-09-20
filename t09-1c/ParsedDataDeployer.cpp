bb270bd2f48fc2f96741f86b18271e3c694e9eb0
Luigi Cham <luigiccham@gmail.com>
2014-09-20 19:09:09 +0800

    Added ParsedDataDeployer class

---
+++ t09-1c/ParsedDataDeployer.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "ParsedDataDeployer.h"
+
+
+ParsedDataDeployer::ParsedDataDeployer()
+{
+}
+
+
+ParsedDataDeployer::~ParsedDataDeployer()
+{
+}
---
+++ t09-1c/ParsedDataDeployer.h
@@ -0,0 +1,8 @@
+#pragma once
+class ParsedDataDeployer
+{
+public:
+	ParsedDataDeployer();
+	~ParsedDataDeployer();
+};
+
