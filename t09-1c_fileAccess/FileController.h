da0cb70ae7bec39c638ad2439adf8221ecce5bcc
Luigi Cham <luigiccham@gmail.com>
2014-09-21 12:46:49 +0800

    file Access code Alpha stage

--- t09-1c_fileAccess/FileController.h
+++ t09-1c_fileAccess/FileController.h
@@ -1,8 +1,29 @@
 #pragma once
+#ifndef FILE_CONTROLLER
+#define FILE_CONTROLLER
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+
+#define BAD_ACCESS \
+	"Error. File cannot be accessed."
+
+#endif
+
+using namespace std;
+
 class FileController
 {
-public:
+private:
 	FileController();
 	~FileController();
+
+public:
+	static bool checkFile(string fileName);
+	static vector<string> parseFileToMemoryVector(string fileName);
+	static bool cloneMemoryVectorToFile(string fileName, vector<string> memVector);
+	static string declareFileAccessError();
 };
 
