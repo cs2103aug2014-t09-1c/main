f18686d48d81be7716d59013d3d30ea0ba04297f
Luigi Cham <luigiccham@gmail.com>
2014-09-27 13:39:36 +0800

    TimeLogic implemented

--- t09-1c_logic/TimeLogic.h
+++ t09-1c_logic/TimeLogic.h
@@ -3,6 +3,7 @@
 #define TIME_LOGIC
 
 #include <time.h>
+#include <ctime>
 #include <iostream>
 
 using namespace std;
@@ -10,11 +11,46 @@ using namespace std;
 class TimeLogic
 {
 private:
-	int year, month, day, hour, min;
+	bool timeFormatCheck = true;
+	string stringTime;
+	
+	//Facts of life:
+	const int TIME_DATE_LENGTH_FORMAT = 2;
+	const int YEAR_LENGTH_FORMAT = 4;
+	const int MONTHS_IN_YEAR = 12;
+	const int DAYS_IN_31MONTH = 31;
+	const int DAYS_IN_30MONTH = 30;
+	const int DAYS_IN_FEB = 28;
+	const int DAYS_IN_FEB_LEAP = 29;
+	const int HOURS_IN_DAY = 24;
+	const int MINUTES_IN_HOUR = 60;
+	
+	//indicates positioning in agreed format "dd/mm/yyyy HH:MM"
+	const int DATE_POSITION = 0;
+	const int MONTH_POSITION = 3;
+	const int YEAR_POSITION = 6;
+	const int HOUR_POSITION = 11;
+	const int MIN_POSITION = 14;
+	
+	//Set-Up TimeLogic Functions
+	void convertStringToTimeLogic(string timeString);
+	bool isParameterStringEmpty(string parameter);
+	bool isParameterStringANumber(string parameter);
+	int convertStringToInt(string argument);
+	int convertStringToYear(string yearString);
+	int convertStringToMonth(string monthString);
+	int convertStringToDay(string dayString);
+	int convertStringToHour(string hourString);
+	int convertStringToMinutes(string minuteString);
+
 public:
-	TimeLogic(string time); //time should be in "dd/mm/yy HH:MM" format. NO EXCEPTIONS.
+	int year, month, day, hour, min;
+	TimeLogic(string time); //time should be in "dd/mm/yyyy HH:MM" format. NO EXCEPTIONS.
 	~TimeLogic();
+	bool getTimeFormatCheck();
 	void declareTimeFormatError();
+	static bool isFirstEarlierThanSecond(TimeLogic time1, TimeLogic time2);
+	static bool isTimeBeforeToday(TimeLogic time1);
 };
 
 #endif
--- t09-1c_parser/ParsedDataPackage.h
+++ t09-1c_parser/ParsedDataPackage.h
@@ -14,8 +14,8 @@ public:
 	//all data related to event info to be stored are represented here.
 	string name;
 	int rating;
-	string start; //FORMAT SHOULD BE dd/mm/yy HH:MM. NO EXCEPTIONS. MAKE SURE PARSER DOES THIS.
-	string end; //FORMAT SHOULD BE dd/mm/yy HH:MM. NO EXCEPTIONS. MAKE SURE PARSER DOES THIS.
+	string start; //FORMAT SHOULD BE "dd/mm/yyyy HH:MM". NO EXCEPTIONS. MAKE SURE PARSER DOES THIS.
+	string end; //FORMAT SHOULD BE "dd/mm/yyyy HH:MM". NO EXCEPTIONS. MAKE SURE PARSER DOES THIS.
 	string category;
 	string type;
 
