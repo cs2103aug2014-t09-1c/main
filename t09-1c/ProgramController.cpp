2737e4ab7092f70de29ea9402362b96ae96f87ee
JoThZh <jaeteezack@gmail.com>
2014-09-29 22:45:29 +0800

    In ProgramController added SendToParser & SendToLogic methods

--- t09-1c/ProgramController.cpp
+++ t09-1c/ProgramController.cpp
@@ -2,6 +2,9 @@
 #include "ProgramController.h"
 #include "ParsedDataDeployer.h"
 
+//after an input is scanned by UI method, call to method sendToParse is made to send input to Parser
+//after Parser returns the variables in ParsedDataPackage, send the details to logic
+//after file output returns an output, controller sends it to UI
 
 ProgramController::ProgramController()
 {
@@ -11,9 +14,38 @@ ProgramController::ProgramController()
 ProgramController::~ProgramController()
 {
 }
-void ProgramController::SendToLogic(string parsedString) //readable format
+
+
+
+string ProgramController::SendToParser(string input)//placeholder input for scanned input from UI
 {
-	NewLogic::addNewLogic(parsedString);
+	//if command = add, delete, or edit, use parseAndReturn accordingly
+	command = CommandAndArgumentParser::getCommand(input); //constructor needed or just static?
+	arguments = CommandAndArgumentParser::getArguments(input);
+	CommandAndArgumentParser::commandParser(command, arguments);
+
+	if (command == "add"){
+		parseOutput = AddParser::parseAndReturn(string arguments);
+	}
+	else if (command == "edit"){
+		parseOutput = EditParser::parseAndReturn(string arguments);
+	}
+	else if (command == "delete"){
+		parseOutput = DeleteParser::parseAndReturn(string arguments);
+	}
+}
+void ProgramController::SendToLogic() 
+{
+	if (command == "add"){	
+		BaseClassLogic::addParsedOutput(parseOutput);
+	}
+	else if (command == "edit"){
+		BaseClassLogic::editParsedOutput(parseOutput);
+	}
+	else if (command == "delete"){
+		BaseClassLogic::deleteParsedOutput(parseOutput);
+	}
+	
 }
 void ProgramController::getOutput(string)
 {
--- t09-1c/ProgramController.h
+++ t09-1c/ProgramController.h
@@ -2,12 +2,20 @@
 #include <string>
 #include <sstream>
 #include "ParsedDataDeployer.h"
+#include "CommandAndArgumentParser.h"
+#include "AddParser.h"
+#include "ParsedDataPackage.h"
+ParsedDataPackage parseOutput;
+string command;
+string arguments;
 
 class ProgramController
 {
 public:
 	ProgramController();
 	~ProgramController();
-	void ProgramController::SendToLogic(string parsedString);
+	void SendToLogic(string parsedString);
+	void getOutput(string);
+	string SendToParser(string input);
 };
 
