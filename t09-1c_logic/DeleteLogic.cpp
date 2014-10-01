7820e6e5250fcc41a62c09ba1d4a92a5eee9e6c8
TCTC <tiance93tia@gmail.com>
2014-10-01 16:08:38 +0800

    deleteLogic

--- t09-1c_logic/DeleteLogic.cpp
+++ t09-1c_logic/DeleteLogic.cpp
@@ -2,9 +2,35 @@
 #include "DeleteLogic.h"
 #include <string>
 
-void DeleteLogic::deleteUserInputRelatedMessage(string userInput, string filename)
+
+void DeleteLogic::deleteLogic(int position, string filename) : fileHander(filename)
 {
-	if ()
+	FILE *pFile;
+	static char tempLine[];//temperary array with undefined size to store each line of undeleted data.
+	char *bufferToTakeInData;
+	char *ptr;
+	int fileSize = FileLogic::getSize();
+
+	bufferToTakeInData = (char*)malloc(fileSize*sizeof(char));//I try to use malloc, but dont know
+	memset(bufferToTakeInData,0,fileSize*sizeof(char); //how to determine the size of this buffer
+	ptr = bufferToTakeInData;
+
+	string DataToBeDeleted = FileLogic::getLineFromPositionNumber(position);
+
+	pFile = fopen(filename);// How to access the file here?
+
+	if (pFile != NULL){
+		while (!feof(pFile)){
+			fgets(tempLine,fileSize,pFile); //How to identify the size?
+			if (strcmp(tempLine, DataToBeDeleted) != 0){
+				strcpy(ptr,tempLine); //put tempLine to buffer
+			}
+		}
+		fclose(pFile);
+
+		pFile = fopen();
+		fprintf(pFile,"%s", bufferToTakeInData);
+	}
 }
 
 
