52ea38f5665f8118bc7abdbe7bf7d31e137322b4
JoThZh <jaeteezack@gmail.com>
2014-10-22 23:27:23 +0800

    Trial Unit Tests 2

--- t09-1c_test/UnitTestSearch.cpp
+++ t09-1c_test/UnitTestSearch.cpp
@@ -17,7 +17,15 @@ namespace t091c_test
 			string expectedArgument = "111014";
 			Assert::AreEqual(expectedArgument,argument);
 		}
-		TEST_METHOD(TestMethod2)
+		TEST_METHOD(TestMethod2a)
+		{
+			SearchParser test;
+			string argument = test.findTypeAndGetArgument("date 11101");
+			test.extractDate(argument);
+			string errorString = SEARCH_PARSER_6DIGIT_DATE_ERROR;
+			Assert::AreEqual(errorString, test.getErrorString());
+		}
+		TEST_METHOD(TestMethod2b)
 		{
 			SearchParser test;
 			string argument = test.findTypeAndGetArgument("date 1110141");
@@ -25,13 +33,29 @@ namespace t091c_test
 			string errorString = SEARCH_PARSER_6DIGIT_DATE_ERROR;
 			Assert::AreEqual(errorString, test.getErrorString());
 		}
-		TEST_METHOD(TestMethod3)
+		TEST_METHOD(TestMethod2c)
 		{
 			SearchParser test;
 			string argument = test.findTypeAndGetArgument("date 111014  ");
 			string date = test.extractDate(argument);
 			string testDate = "11/10/14";
 			Assert::AreEqual(testDate, date);
+		}
+		TEST_METHOD(TestMethod3a)
+		{
+			SearchParser test;
+			string argument = test.findTypeAndGetArgument("date Monday");
+			string date = test.extractDate(argument);
+			string testDate = "27/10/14";
+			Assert::AreEqual(testDate, date);
+		}
+		TEST_METHOD(TestMethod3b)
+		{
+			SearchParser test;
+			string argument = test.findTypeAndGetArgument("date Grunday");
+			test.extractDate(argument);
+			string errorString = SEARCH_PARSER_DAY_OF_WEEK_ERROR;
+			Assert::AreEqual(errorString, test.getErrorString());
 		}
 		/*ParsedDataPackage SearchParser::parseAndReturn(string parseInput)
 		{
