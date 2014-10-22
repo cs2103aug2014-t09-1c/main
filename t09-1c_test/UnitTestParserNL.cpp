e7fce08ae1721605dd96078b5e8f00e20dc25bb2
ericszf <ericszf@gmail.com>
2014-10-23 03:03:05 +0800

    Updated Test Cases for ParserNL

--- t09-1c_test/UnitTestParserNL.cpp
+++ t09-1c_test/UnitTestParserNL.cpp
@@ -67,6 +67,7 @@ namespace t091c_test
 			//Assert::AreEqual(expectedName, parsedData.name);
 		}
 
+		// This is a test case for float syntax partition
 		TEST_METHOD(NameTest1)
 		{
 			AddParser add;
@@ -75,6 +76,51 @@ namespace t091c_test
 			string expectedTime = "";
 			string expectedDate = "";
 			string expectedName = "meeting with boss";
+			//Assert::AreEqual(expectedCategory, parsedData.category);
+			//Assert::AreEqual(expectedTime, parsedData.start);
+			//Assert::AreEqual(expectedDate, parsedData.date);
+			Assert::AreEqual(expectedName, parsedData.name);
+		}
+
+		// This is a test case for float syntax partition on keywords
+		TEST_METHOD(NameTest2)
+		{
+			AddParser add;
+			ParsedDataPackage parsedData = add.parseNLandReturn("meeting at NUS Starbucks");
+			string expectedCategory = "";
+			string expectedTime = "";
+			string expectedDate = "";
+			string expectedName = "meeting at NUS Starbucks";
+			//Assert::AreEqual(expectedCategory, parsedData.category);
+			//Assert::AreEqual(expectedTime, parsedData.start);
+			//Assert::AreEqual(expectedDate, parsedData.date);
+			Assert::AreEqual(expectedName, parsedData.name);
+		}
+
+		// This is a test case for float syntax partition on keywords
+		TEST_METHOD(NameTest3)
+		{
+			AddParser add;
+			ParsedDataPackage parsedData = add.parseNLandReturn("Surf on 13 seas");
+			string expectedCategory = "";
+			string expectedTime = "";
+			string expectedDate = "";
+			string expectedName = "Surf on 13 seas";
+			//Assert::AreEqual(expectedCategory, parsedData.category);
+			//Assert::AreEqual(expectedTime, parsedData.start);
+			//Assert::AreEqual(expectedDate, parsedData.date);
+			Assert::AreEqual(expectedName, parsedData.name);
+		}
+
+		// This is a test case for float syntax partition on keywords
+		TEST_METHOD(NameTest4)
+		{
+			AddParser add;
+			ParsedDataPackage parsedData = add.parseNLandReturn("from the rising sun at 10342 street on Sundays");
+			string expectedCategory = "";
+			string expectedTime = "";
+			string expectedDate = "";
+			string expectedName = "from the rising sun at 10342 street on Sundays";
 			//Assert::AreEqual(expectedCategory, parsedData.category);
 			//Assert::AreEqual(expectedTime, parsedData.start);
 			//Assert::AreEqual(expectedDate, parsedData.date);
