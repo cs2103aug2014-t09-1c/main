6df2ae0f38d5ebc1fd47c13d322f8d1591dc3d11
JoThZh <jaeteezack@gmail.com>
2014-10-31 00:14:41 +0800

    merge

--- t09-1c_test/UnitTestSearch.cpp
+++ t09-1c_test/UnitTestSearch.cpp
@@ -13,14 +13,14 @@ namespace t091c_test
 		TEST_METHOD(TestMethod1)
 		{
 			SearchParser test;
-			string argument = test.findTypeAndGetArgument("date 111014");
+			string argument = test.findTypeAndGetArgument("date 111014");//equivalence partitioning for valid values
 			string expectedArgument = "111014";
 			Assert::AreEqual(expectedArgument,argument);
 		}
 		TEST_METHOD(TestMethod2a)
 		{
 			SearchParser test;
-			string argument = test.findTypeAndGetArgument("date 11101");
+			string argument = test.findTypeAndGetArgument("date 11101");//equivalence partitioning for 
 			test.extractDate(argument);
 			string errorString = SEARCH_PARSER_6DIGIT_DATE_ERROR;
 			Assert::AreEqual(errorString, test.getErrorString());
@@ -28,7 +28,7 @@ namespace t091c_test
 		TEST_METHOD(TestMethod2b)
 		{
 			SearchParser test;
-			string argument = test.findTypeAndGetArgument("date 1110141");
+			string argument = test.findTypeAndGetArgument("date 1110141");//for longer strings
 			test.extractDate(argument);
 			string errorString = SEARCH_PARSER_6DIGIT_DATE_ERROR;
 			Assert::AreEqual(errorString, test.getErrorString());
@@ -36,23 +36,23 @@ namespace t091c_test
 		TEST_METHOD(TestMethod2c)
 		{
 			SearchParser test;
-			string argument = test.findTypeAndGetArgument("date 111014  ");
+			string argument = test.findTypeAndGetArgument("date 111014  ");//for white space
 			string date = test.extractDate(argument);
-			string testDate = "11/10/14";
+			string testDate = "11/10/2014";
 			Assert::AreEqual(testDate, date);
 		}
 		TEST_METHOD(TestMethod3a)
 		{
 			SearchParser test;
-			string argument = test.findTypeAndGetArgument("date Monday");
+			string argument = test.findTypeAndGetArgument("date Monday");//have to repeat for all valid
 			string date = test.extractDate(argument);
-			string testDate = "27/10/14";
+			string testDate = "27/10/2014";
 			Assert::AreEqual(testDate, date);
 		}
 		TEST_METHOD(TestMethod3b)
 		{
 			SearchParser test;
-			string argument = test.findTypeAndGetArgument("date Grunday");
+			string argument = test.findTypeAndGetArgument("date Grunday");//for all wrong strings
 			test.extractDate(argument);
 			string errorString = SEARCH_PARSER_DAY_OF_WEEK_ERROR;
 			Assert::AreEqual(errorString, test.getErrorString());
